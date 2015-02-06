00afa78081a97347062ae0740afff61fb0c1a152
fcazorlay <fcazorlay@gmail.com>
2015-02-06 20:29:22 +0100

    Math is Fun 7.0
    Setè exercici del projecte Math is Fun

---
+++ Math_is_fun7.0/Math_is_fun7.0.ino
@@ -0,0 +1,30 @@
+/************************************************************************
+**                                                                     **
+**                        Descripció programa                          **
+**                                                                     **
+/***********************************************************************/
+
+//******  Inculdes  *****************************************************
+
+
+//******  Variables  ****************************************************
+
+//******  Setup  ********************************************************
+int test = 32767;
+
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     //set up Serial library at 9600 bps
+  Serial.print("Test value is: ");
+  Serial.println(test);
+
+  test = test + 1;
+ 
+  Serial.print("Now it is ");
+  Serial.println(test);
+}
+
+void loop()   // we need this to be here even though its empty
+{
+}
+
