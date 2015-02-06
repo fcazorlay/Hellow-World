105e91cba0e3bba5c46238604af9e5d23ab42922
fcazorlay <fcazorlay@gmail.com>
2015-02-06 20:45:47 +0100

    Math is Fun 10.0
    Desè exercici del projecte Math is Fun

---
+++ Math_is_fun10.0/Math_is_fun10.0.ino
@@ -0,0 +1,47 @@
+/************************************************************************
+**                                                                     **
+**                        Descripció programa                          **
+**                                                                     **
+/***********************************************************************/
+
+//******  Inculdes  *****************************************************
+
+
+//******  Variables  ****************************************************
+
+//******  Setup  ********************************************************
+int drive_gb = 100;
+long drive_mb;
+long drive_kb;
+long real_drive_mb;
+long real_drive_kb;
+
+void setup()               //run once, when the sketch starts
+{
+  Serial.begin(9600);       //set up Serial library at 9600 bps
+
+  Serial.print("Your HD is ");
+  Serial.print(drive_gb);
+  Serial.println(" GB large.");
+  Serial.print("In theory, it can store")
+  Serial.println(drive_mb);
+  
+  drive_mb = drive_gb;
+  drive_mb = drive_mb * 1024;
+  
+  Serial.print("Megabytes,");
+  
+  drive_mb = drive_kb;
+  drive_mb = drive_mb * 1024;
+  
+  Serial.print("Kilobites");
+
+  Serial.print("But it really only stores");
+  Serial.print(drive_mb);
+  Serial.println(" Megabytes!");
+}
+
+void loop()      //we need this to be here even though its empty
+{
+}
+
