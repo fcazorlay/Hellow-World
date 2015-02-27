0ef58d2c3ca29f3751fafddd541ab1dd44a7d073
fcazorlay <fcazorlay@gmail.com>
2015-02-27 20:33:51 +0100

    Control Structures 1.2

---
+++ Control Structures/Control_Structures_1.2/Control_Structures_1.2.ino
@@ -0,0 +1,40 @@
+/************************************************************************
+**                                                                     **
+**                        Descripció programa                          **
+**                                                                     **
+/***********************************************************************/
+
+//******  Inculdes  *****************************************************
+
+
+//******  Variables  ****************************************************
+
+//******  Setup  ********************************************************
+int tempAigua = 100;
+
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  if ( tempAigua < 90)
+  {
+    Serial.print("Aigua encara no bull");
+  }
+ else if (tempAigua  >= 90 && tempAigua < 100)
+  {
+   Serial.print("Aigua apunt de bullir");
+  }
+ 
+  else if (tempAigua == 100)
+  {
+  Serial.print("Aigua a 100 graus");
+  }
+  else
+  {
+    Serial.print("Aigua bullint");
+  }
+}
+void loop()   // we need this to be here even though its empty
+{
+}
+
